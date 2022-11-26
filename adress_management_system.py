04bf4c56196b0a8a29c349c817638c4b7922d050
Nico Schurig <nico-schurig@gmx.de>
2022-11-26 16:29:35 +0100

    Adding Entry Widgets

--- adress_management_system.py
+++ adress_management_system.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 
 
-
+# ================================== GUI =======================================================
 root = tk.Tk()
 root.geometry("1000x600")
 root.title("Address Management System")
@@ -10,6 +10,7 @@ root.title("Address Management System")
 title_label = ttk.Label(root, text = "Address Management System", font = (None, 40))
 title_label.pack()
 
+# Data Labels:
 name_label = ttk.Label(root, text = "Name:", font = (None, 20), compound = "left")
 name_label.place(x = 10, y = 80)
 
@@ -21,5 +22,19 @@ email_address_label.place(x = 10, y = 140)
 
 birthday_label = ttk.Label(root, text = "Birthday:", font = (None, 20), compound = "left")
 birthday_label.place(x = 10, y = 170)
+
+# Entry Widgets:
+name_entry = ttk.Entry(root)
+name_entry.place(x = 150, y = 80)
+
+firstname_entry = ttk.Entry(root)
+firstname_entry.place(x = 150, y = 110)
+
+email_address_entry = ttk.Entry(root)
+email_address_entry.place(x = 150, y = 140)
+
+birthday_entry = ttk.Entry(root)
+birthday_entry.place(x = 150, y = 170)
+
 
 root.mainloop()
