f9fa516f1a1b91b6c76fba4c15a8d3ab52381079
Nico Schurig <nico-schurig@gmx.de>
2022-12-01 21:46:00 +0100

    implemented class structure for GUI

--- adress_management_system.py
+++ adress_management_system.py
@@ -1,12 +1,8 @@
-from importlib.metadata import entry_points
 import tkinter as tk
 import pickle
 from tkinter import ttk, messagebox
 from tkinter import *
-from tkinter.messagebox import showinfo
 import os
-
-from jinja2 import select_autoescape
 
 # ================================= Classes ======================================================
 class Person():
@@ -18,251 +14,245 @@ class Person():
         self._month = month
         self._year = year
 
-# ================================ Functions ===================================================
-person_list = []
+class AddressManager():
+    #Constructor:
+    def __init__(self, file_name, root):
+        #count variable for iid
+        self.count = 0
 
-if (os.stat("data.bin").st_size != 0):
-    person_pickle = open("data.bin", "rb")
-    person_list = pickle.load(person_pickle)
-    person_pickle.close()
+        self._filename = file_name
 
-def check_for_valid_entry():
-    if(len(name_entry.get()) != 0 and 
-        len(firstname_entry.get()) != 0 and  
-        len(email_address_entry.get()) != 0 and 
-        len(birthday_entry.get()) != 0):
-    
-        return True
-    
-    else:
-        #returns a statement in the concole, if atleast one entry widget is empty
-        print("You have to enter a value in each entry field!")
-        return False
+        #displays Headline at the center-top of the window
+        self.title_label = ttk.Label(root, text = "Address Management System", font = (None, 40))
+        self.title_label.place(x = 175)
+
+        # Data Labels:
+        self.name_label = ttk.Label(root, text = "Name:", font = (None, 20), compound = "left")
+        self.name_label.place(x = 10, y = 80)
+
+        self.firstname_label = ttk.Label(root, text = "First Name:", font = (None, 20), compound = "left")
+        self.firstname_label.place(x = 10, y = 110)
+
+        self.email_address_label = ttk.Label(root, text = "E-Mail:", font = (None, 20), compound = "left")
+        self.email_address_label.place(x = 10, y = 140)
+
+        self.birthday_label = ttk.Label(root, text = "Birthday:", font = (None, 20), compound = "left")
+        self.birthday_label.place(x = 10, y = 170)
+
+        # Entry Widgets:
+        self.name_entry = ttk.Entry(root)
+        self.name_entry.place(x = 150, y = 80)
+
+        self.firstname_entry = ttk.Entry(root)
+        self.firstname_entry.place(x = 150, y = 110)
+
+        self.email_address_entry = ttk.Entry(root)
+        self.email_address_entry.place(x = 150, y = 140)
+
+        self.birthday_entry = ttk.Entry(root)
+        self.birthday_entry.place(x = 150, y = 170)
+
+        # Buttons:
+        self.add_button = ttk.Button(root, text = "Add", width = 10, command = self.add)
+        self.add_button.place(x = 30, y = 220)
+
+        self.Update_button = ttk.Button(root, text = "Update", width = 10, command = self.update)
+        self.Update_button.place(x = 170, y = 220)
+
+        self.Delete_button = ttk.Button(root, text = "Delete", width = 10, command = self.delete)
+        self.Delete_button.place(x = 310, y = 220)
+
+        # Listbox:
+        # Headlines of the table:
+        self.columns_tupel = ("Name", "First Name", "E-Mail", "Birthday" )
+        self.listBox = ttk.Treeview(root, columns = self.columns_tupel, show = "headings")
+
+        #Formatting the columns of the table
+        for col in self.columns_tupel:
+            self.listBox.heading(col, text = col)
+            self.listBox.grid(row = 1, column = 0, columnspan = 2)
+            self.listBox.place(x = 10, y = 300)
 
 
-def add():
-    
-    #check if each entry widget contains a value
-    if (check_for_valid_entry()):
-        #Read data from file, stored it in person_list
-        person_pickle = open("data.bin", "rb")
+        #Defines what happens, when a list record is selected
+        self.listBox.bind('<ButtonRelease-1>', self.item_selected)
+
+        self.show()
+
+    #Methods:
+    def read_data(self):                                #reads data from file, given by name (_filename)
+        person_pickle = open(self._filename, "rb")
         person_list = pickle.load(person_pickle)
         person_pickle.close()
-        
-        #Add the new Person to the end of the person_list
-        person_list.append(Person(name_entry.get(), 
-                                    firstname_entry.get(), 
-                                    email_address_entry.get(),
-                                    transform_birthday(birthday_entry.get(), "d"),
-                                    transform_birthday(birthday_entry.get(), "m"),
-                                    transform_birthday(birthday_entry.get(), "y")))
 
-        #Write updated person_list in the data file
-        person_pickle = open("Data.bin", "wb")
+        return person_list
+
+    def write_data(self, person_list):                  #writes data into file given by name (_filename)
+        person_pickle = open(self._filename, "wb")
         pickle.dump(person_list, person_pickle)
         person_pickle.close()
 
-        #Display the new Record
-        listBox.insert(parent = "", index = END, iid = 0, text = "", values = (name_entry.get(),
-                                                                                firstname_entry.get(),
-                                                                                email_address_entry.get(),
-                                                                                birthday_entry.get()))
-
-        #Clear entry widgets
-        name_entry.delete(0, END)
-        firstname_entry.delete(0, END)
-        email_address_entry.delete(0, END)
-        birthday_entry.delete(0, END)
-
-def delete():
-    #Get record number
-    selected_iid = listBox.focus()
-
-    #Read data from file, stored it in person_list
-    person_pickle = open("data.bin", "rb")
-    person_list = pickle.load(person_pickle)
-    person_pickle.close()
-
-    #Get index of changed data record
-    item_index = listBox.index(selected_iid)
-
-    #Remove data from file at index of changed data record
-    del person_list[item_index]
-
-    #Write updated person_list in the data file
-    person_pickle = open("Data.bin", "wb")
-    pickle.dump(person_list, person_pickle)
-    person_pickle.close()
-
-    #Remove record
-    selected_record = listBox.selection()[0]
-    listBox.delete(selected_record)
-
-    #Clear entry widgets
-    name_entry.delete(0, END)
-    firstname_entry.delete(0, END)
-    email_address_entry.delete(0, END)
-    birthday_entry.delete(0, END)
-
-
-def item_selected(e):
-    #Clear entry widgets
-    name_entry.delete(0, END)
-    firstname_entry.delete(0, END)
-    email_address_entry.delete(0, END)
-    birthday_entry.delete(0, END)
-
-    #Get record number
-    selected_iid = listBox.focus()
-
-    #Get record values
-    values = listBox.item(selected_iid, "values")
-
-    #Ouput to entry widget
-    name_entry.insert(0, values[0])
-    firstname_entry.insert(0, values[1])
-    email_address_entry.insert(0, values[2])
-    birthday_entry.insert(0, values[3])
+    def clear_entry_widgets(self):                      #deletes text out of the entry widgets
+        self.name_entry.delete(0, END)
+        self.firstname_entry.delete(0, END)
+        self.email_address_entry.delete(0, END)
+        self.birthday_entry.delete(0, END)
 
 
 
-def show():
-    if (os.stat("data.bin").st_size != 0):
-        person_pickle = open("data.bin", "rb")
-        person_list = pickle.load(person_pickle)
-        person_pickle.close()
-
-        for item in listBox.get_children():
-            listBox.delete(item)
-
-        for person in person_list:
-            listBox.insert("", tk.END, values = (str(person._name), str(person._first_name), str(person._email), str(person._day) + "." + str(person._month) + "." + str(person._year)))
-
-
-def transform_birthday(new_birthday, identifier):
-    #returns value of day
-    if identifier == "d":
-        day = str(new_birthday[0:2])
-        return day
-    
-    #returns value of month
-    elif identifier == "m":
-        month = str(new_birthday[3:5])
-        return month
-    
-    #returns value of year
-    elif identifier == "y":
-        year = str(new_birthday[6:10])
-        return year
-    
-    else:
-        return "XX"
+    def add(self):
+        #check if each entry widget contains a value
+        if (self.check_for_valid_entry()):
+            #Read data from file, stored it in person_list
+            person_list = self.read_data()
         
+            #Add the new Person to the end of the person_list
+            person_list.append(Person(self.name_entry.get(), 
+                                    self.firstname_entry.get(), 
+                                    self.email_address_entry.get(),
+                                    self.transform_birthday(self.birthday_entry.get(), "d"),
+                                    self.transform_birthday(self.birthday_entry.get(), "m"),
+                                    self.transform_birthday(self.birthday_entry.get(), "y")))
 
+            
 
+            #Write updated person_list in the data file
+            self.write_data(person_list)
 
-def update():
-    #Get record number
-    selected_iid = listBox.focus()
+            #Display the new Record
+            self.listBox.insert(parent = "", index = END, iid = self.count, text = "", values = (self.name_entry.get(),
+                                                                                self.firstname_entry.get(),
+                                                                                self.email_address_entry.get(),
+                                                                                self.birthday_entry.get()))
+            self.count += 1     #increases the count for the next listBox entry/iid
 
-    #Update record
-    listBox.item(selected_iid, values = (name_entry.get(), 
-                                        firstname_entry.get(), 
-                                        email_address_entry.get(), 
-                                        birthday_entry.get()))
+            self.clear_entry_widgets()
 
-    #Read data from file, store it in person_list
-    person_pickle = open("data.bin", "rb")
-    person_list = pickle.load(person_pickle)
-    person_pickle.close()
+    def delete(self):
+        #Get record number
+        selected_iid = self.listBox.focus()
 
-    #Get index of changed data record
-    record_index = listBox.index(selected_iid)
+        #Read data from file, stored it in person_list
+        person_list = self.read_data()
 
-    #Remove data from file at index of changed data record
-    del person_list[record_index]
+        #Get index of changed data record
+        item_index = self.listBox.index(selected_iid)
 
-    #Write updated data (at the index of the changed data record) in person_list
-    person_list.insert(record_index, Person(name_entry.get(), 
-                                            firstname_entry.get(), 
-                                            email_address_entry.get(), 
-                                            transform_birthday(birthday_entry.get(), "d"),
-                                            transform_birthday(birthday_entry.get(), "m"),
-                                            transform_birthday(birthday_entry.get(), "y")))
+        #Remove data from file at index of changed data record
+        del person_list[item_index]
 
-    #Write updated person_list in the data file
-    person_pickle = open("Data.bin", "wb")
-    pickle.dump(person_list, person_pickle)
-    person_pickle.close()
+        #Write updated person_list in the data file
+        self.write_data(person_list)
 
-    #Clear entry widgets
-    name_entry.delete(0, END)
-    firstname_entry.delete(0, END)
-    email_address_entry.delete(0, END)
-    birthday_entry.delete(0, END)
+        #Remove record
+        selected_record = self.listBox.selection()[0]
+        self.listBox.delete(selected_record)
 
+        self.clear_entry_widgets()
 
-# ================================== GUI =======================================================
-root = tk.Tk()
-root.geometry("830x600")
-root.title("Address Management System")
-root.resizable(0, 0)
+    def update(self):
+        #Get record number
+        selected_iid = self.listBox.focus()
 
-title_label = ttk.Label(root, text = "Address Management System", font = (None, 40))
-title_label.place(x = 175)
+        #Update record
+        self.listBox.item(selected_iid, values = (self.name_entry.get(), 
+                                        self.firstname_entry.get(), 
+                                        self.email_address_entry.get(), 
+                                        self.birthday_entry.get()))
 
-# Data Labels:
-name_label = ttk.Label(root, text = "Name:", font = (None, 20), compound = "left")
-name_label.place(x = 10, y = 80)
+        #Read data from file, store it in person_list
+        person_list = self.data_read()
 
-firstname_label = ttk.Label(root, text = "First Name:", font = (None, 20), compound = "left")
-firstname_label.place(x = 10, y = 110)
+        #Get index of changed data record
+        record_index = self.listBox.index(selected_iid)
 
-email_address_label = ttk.Label(root, text = "E-Mail:", font = (None, 20), compound = "left")
-email_address_label.place(x = 10, y = 140)
+        #Remove data from file at index of changed data record
+        del person_list[record_index]
 
-birthday_label = ttk.Label(root, text = "Birthday:", font = (None, 20), compound = "left")
-birthday_label.place(x = 10, y = 170)
+        #Write updated data (at the index of the changed data record) in person_list
+        person_list.insert(record_index, Person(self.name_entry.get(), 
+                                            self.firstname_entry.get(), 
+                                            self.email_address_entry.get(), 
+                                            self.transform_birthday(self.birthday_entry.get(), "d"),
+                                            self.transform_birthday(self.birthday_entry.get(), "m"),
+                                            self.transform_birthday(self.birthday_entry.get(), "y")))
 
-# Entry Widgets:
-name_entry = ttk.Entry(root)
-name_entry.place(x = 150, y = 80)
+        #Write updated person_list in the data file
+        self.write_data(person_list)
 
-firstname_entry = ttk.Entry(root)
-firstname_entry.place(x = 150, y = 110)
+        self.clear_entry_widgets()
 
-email_address_entry = ttk.Entry(root)
-email_address_entry.place(x = 150, y = 140)
+    def transform_birthday(x, new_birthday, identifier):        #x variable not used
+        #returns value of day
+        if identifier == "d":
+            day = new_birthday[0:2]
+            return day
+    
+        #returns value of month
+        elif identifier == "m":
+            month = new_birthday[3:5]
+            return month
+    
+        #returns value of year
+        elif identifier == "y":
+            year = new_birthday[6:10]
+            return year
+    
+        else:
+            return "XX"
 
-birthday_entry = ttk.Entry(root)
-birthday_entry.place(x = 150, y = 170)
+    def item_selected(e, self):
+        #Clear entry widgets
+        e.name_entry.delete(0, END)
+        e.firstname_entry.delete(0, END)
+        e.email_address_entry.delete(0, END)
+        e.birthday_entry.delete(0, END)
 
-# Buttons:
-add_button = ttk.Button(root, text = "Add", width = 10, command = add)
-add_button.place(x = 30, y = 220)
+        #Get record number
+        selected_iid = e.listBox.focus()
 
-Update_button = ttk.Button(root, text = "Update", width = 10, command = update)
-Update_button.place(x = 170, y = 220)
+        #Get record values
+        values = e.listBox.item(selected_iid, "values")
 
-Delete_button = ttk.Button(root, text = "Delete", width = 10, command = delete)
-Delete_button.place(x = 310, y = 220)
+        #Ouput to entry widget
+        e.name_entry.insert(0, values[0])
+        e.firstname_entry.insert(0, values[1])
+        e.email_address_entry.insert(0, values[2])
+        e.birthday_entry.insert(0, values[3])
 
-# Listbox
-columns_tupel = ("Name", "First Name", "E-Mail", "Birthday" )
-listBox = ttk.Treeview(root, columns = columns_tupel, show = "headings")
+    def check_for_valid_entry(self):
+        #checks if all entry widgets contain content
+        if(len(self.name_entry.get()) != 0 and 
+            len(self.firstname_entry.get()) != 0 and  
+            len(self.email_address_entry.get()) != 0 and 
+            len(self.birthday_entry.get()) != 0):
+    
+            return True
+    
+        else:
+            #returns a statement in the concole, if atleast one entry widget is empty
+            print("You have to enter a value in each entry field!")
+            return False
 
+    #displays data at the start of the GUI
+    def show(self):
+        
+        if (os.stat(self._filename).st_size != 0):
+            person_list = self.read_data()
 
-for col in columns_tupel:
-    listBox.heading(col, text = col)
-    listBox.grid(row = 1, column = 0, columnspan = 2)
-    listBox.place(x = 10, y = 300)
+            for item in self.listBox.get_children():
+                self.listBox.delete(item)
 
+            for person in person_list:
+                self.listBox.insert("", tk.END, values = (str(person._name), str(person._first_name), str(person._email), str(person._day) + "." + str(person._month) + "." + str(person._year)))
 
-#Defines what happens, when a list record is selected
-listBox.bind('<ButtonRelease-1>', item_selected)
+# ======================================================== Program ========================================================
 
-show()  
+root = tk.Tk()                                      #new main-window named root
+root.geometry("830x600")                            #sets size of window (recommended "830x600")
+root.title("Address Management System")             #sets title in the head of the window
+root.resizable(0, 0)                                #window is not resizable -> size can't be changed by dragging with coursor
 
+address_manager = AddressManager("data.bin", root)  #new Object of type AddressManager containing the file name and main window instance as arguments
 
-
-root.mainloop()
+root.mainloop()                                     #lets tkinter run the application in loop, until window gets closed
