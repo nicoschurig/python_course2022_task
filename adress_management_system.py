2d2dfcffb7c601cbe1bf9e536862891d550232d4
Nico Schurig <nico-schurig@gmx.de>
2022-12-16 12:12:06 +0100

    Implemented new class DataManager

--- adress_management_system.py
+++ adress_management_system.py
@@ -54,6 +54,29 @@ class Person():
         def year(self, newyear: int):                #Setter for year
             self._year = newyear
 
+class DataManager():
+    def __init__(self, file_name):
+        self._file_name = file_name                     #name of file, containing the data
+
+    @property
+    def filename(self) -> str:
+        return self._file_name
+
+    @filename.setter
+    def filename(self, newfilename: str):
+        self._file_name = newfilename
+
+    def read_data(self):                                #reads data from file, given by name (_file_name)
+        person_pickle = open(self._file_name, "rb")
+        person_list = pickle.load(person_pickle)
+        person_pickle.close()
+
+        return person_list
+
+    def write_data(self, person_list):                  #writes data into file given by name (_file_name)
+        person_pickle = open(self._file_name, "wb")
+        pickle.dump(person_list, person_pickle)
+        person_pickle.close()
 
 
 class AddressManager():
