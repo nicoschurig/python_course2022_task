c229e13ff0f0dd83ef39b56fee68f8ffac3bd110
Nico Schurig <nico-schurig@gmx.de>
2022-12-16 11:12:39 +0100

    changed properties (getter/setter)

--- adress_management_system.py
+++ adress_management_system.py
@@ -15,34 +15,43 @@ class Person():
         self._year = year
 
         @property
-        def getName(self) -> str:           #Getter for name
+        def name(self) -> str:           #Getter for name
             return self._name
         
-        def setName(self, newname: str):    #Setter for name
+        @name.setter
+        def name(self, newname: str):    #Setter for name
             self._name = newname
 
-        def getFirstName(self) -> str:                  #Getter for first name
+        @property
+        def first_name(self) -> str:                  #Getter for first name
             return self._first_name
 
-        def setFirstName(self, newfirstname: str):      #Setter for first name
+        @first_name.setter
+        def first_name(self, newfirstname: str):      #Setter for first name
             self._first_name = newfirstname
 
-        def getDay(self) -> int:                        #Getter for day
+        @property
+        def day(self) -> int:                        #Getter for day
             return self._day
 
-        def setDay(self, newday: int):                  #Setter for day
+        @day.setter
+        def day(self, newday: int):                  #Setter for day
             self._day = newday
 
-        def getMonth(self) -> int:                      #Getter for month
+        @property
+        def month(self) -> int:                      #Getter for month
             return self._month
 
-        def setMonth(self, newmonth: int):              #Setter for month
+        @month.setter
+        def month(self, newmonth: int):              #Setter for month
             self._month = newmonth
-
-        def getYear(self) -> int:                       #Getter for year
+        
+        @property
+        def year(self) -> int:                       #Getter for year
             return self._year
         
-        def setYear(self, newyear: int):                #Setter for year
+        @year.setter
+        def year(self, newyear: int):                #Setter for year
             self._year = newyear
 
 
